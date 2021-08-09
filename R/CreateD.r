fc637918b33e9b8adb65a556b8fff7f29b585193
Victor Oribamise <80556557+Victor-Oribamise@users.noreply.github.com>
2021-08-09 17:04:14 -0400

    Update and rename CreateD_Oribamise.r to CreateD.r

--- R/CreateD.r
+++ R/CreateD.r
@@ -1,4 +1,4 @@
-Rcpp::sourceCpp('makeD.cpp')
+Rcpp::sourceCpp('createD.cpp')
 library(Matrix)
 Ped <- structure(list(ID = c(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 
                              11L, 12L, 13L, 14L, 18L, 15L, 16L, 17L, 20L, 19L), Sire = c(NA, 
